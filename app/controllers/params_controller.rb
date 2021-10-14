2291774e4ba29b33c3d64d84de3fe5f1dcd48530
Ian S <iansilber32@gmail.com>
2021-10-14 14:10:20 -0400

    adds number sum route as well as refactors old code to use wildcard method of routing

--- app/controllers/params_controller.rb
+++ app/controllers/params_controller.rb
@@ -1,17 +1,19 @@
 class ParamsController < ApplicationController
   def game
-    guess = params["guess"].to_i
+    guess = params["wildcard"].to_i
     number = 36
     if guess == number
-      render json: {message: "good job! #{guess} was the correct number"}
+      output = {message: "good job! #{guess} was the correct number"}
     elsif guess < number
-      render json: {message: "ooooo too low try again"}
+      output = {message: "ooooo too low try again"}
     elsif guess > number 
-      render json: {message: "ooooo too high try again"}
+      output = {message: "ooooo too high try again"}
     end
+    render json: output
   end
+
   def name
-    name = params["name"]
+    name = params["wildcard"]
     #render json: name.upcase
     if name[0] == "a"
       message = {message: "HEY YOUR NAME STARTS WITH THE FIRST LETTER OF THE ALPHABET!!!"}
@@ -19,5 +21,12 @@ class ParamsController < ApplicationController
       message = {message: "cool name, ig"}
     end
     render json: message
+  end
+
+  def sum
+    num1 = params["num1"].to_i
+    num2 = params["num2"].to_i
+    sum = num1 + num2
+    render json: {message: "the sum of your numbers is #{sum}"}
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get "/game" => "params#game"
-  get "/name" => "params#name"
+  get "/game/:wildcard" => "params#game"
+  get "/name/:wildcard" => "params#name"
+  get "/sum/:num1/:num2" => "params#sum"
 end
