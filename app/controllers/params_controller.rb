0623d1f701faff1200ed1fe4e64ebce050d0dca0
Ian S <iansilber32@gmail.com>
2021-10-14 13:33:45 -0400

    completes exercise adding params query guessing game and name checker

--- app/controllers/params_controller.rb
+++ app/controllers/params_controller.rb
@@ -1,2 +1,23 @@
 class ParamsController < ApplicationController
+  def game
+    guess = params["guess"].to_i
+    number = 36
+    if guess == number
+      render json: {message: "good job! #{guess} was the correct number"}
+    elsif guess < number
+      render json: {message: "ooooo too low try again"}
+    elsif guess > number 
+      render json: {message: "ooooo too high try again"}
+    end
+  end
+  def name
+    name = params["name"]
+    #render json: name.upcase
+    if name[0] == "a"
+      message = {message: "HEY YOUR NAME STARTS WITH THE FIRST LETTER OF THE ALPHABET!!!"}
+    else
+      message = {message: "cool name, ig"}
+    end
+    render json: message
+  end
 end
